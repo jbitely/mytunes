e75a72fd312b8a6d1fba210777950a528802653f
Justin Bitely <jbitely@gmail.com>
2016-01-28 18:12:20 -0500

    Changed listener to listen for 'add remove' instead of 'add dequeue ended'.

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -4,7 +4,7 @@ var SongQueueView = Backbone.View.extend({
   tagName: "table",
 
   initialize: function() {
-    this.collection.on('add dequeue ended', this.render, this);
+    this.collection.on('add remove', this.render, this);
     this.render();
   },
 
